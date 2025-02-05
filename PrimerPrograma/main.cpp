60282e630b51938cabf7cf55529c7cb98c8b00f2
crigrii <juanjolova2006@javerianacali.edu.co>
2025-02-04 19:02:38 -0500

    Añadiendo los archivos del c++

---
+++ PrimerPrograma/main.cpp
@@ -0,0 +1,19 @@
+// Proyecto: PrimerPrograma
+// Archivo: main.cpp
+// Este programa solicita al usuario dos números enteros.
+// Luego calcula y muestra su suma
+// Programador: Profesorcito
+// Fecha: 27/ene/2025
+
+#include <iostream>
+
+int main() {
+    int valor1, valor2; // Manteniendo el tipo int
+    std::cout << "Introduce dos valores enteros: " << std::endl;
+    std::cin >> valor1 >> valor2;
+    std::cout << "La suma de " << valor1 << " y " << valor2 << " es " << (valor1 + valor2) << std::endl;
+    return 0;
+}// TIP See CLion help at <a
+// href="https://www.jetbrains.com/help/clion/">jetbrains.com/help/clion/</a>.
+//  Also, you can try interactive lessons for CLion by selecting
+//  'Help | Learn IDE Features' from the main menu.
